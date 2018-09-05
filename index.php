9a91edffc01bd00fed06804513028847983576e0
Jidu Lal <jidulalvm28@gmail.com>
2018-09-05 09:46:23 +0300

    Changes

--- index.php
+++ index.php
@@ -6,11 +6,14 @@ $url = parse_url(getenv('SCALINGO_INFLUX_URL'));
 
 $client = new \crodas\InfluxPHP\Client($url['host'], $url['port'], $url['user'], $url['pass']);
 
-$dbs = $client->getDatabases();
-if ($dbs) {
-	foreach ((array)$dbs as $db) {
-		echo"<pre>"; print_r($db->getName()); echo"</pre>";
-    	
-	}
+$db = $client->substr($url['path'], 1);
+$db->insert("foo", ['foobar' => 'bar']);
 
+
+
+$result = $client->foobar;
+
+foreach ($result->query("SELECT * FROM foo;") as $row) {
+    var_dump($row, $row->time);
+	echo "<pre>"; print_r($row); echo "</pre><br>";
 }
